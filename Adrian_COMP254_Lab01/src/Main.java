544e70253fced0b8d4b14e1d6b9fd8cb3238d171
adriandot1 <adriandot1@users.noreply.github.com>
2026-01-30 13:44:53 -0500

    Finished DLL, Starting SLL

--- Adrian_COMP254_Lab01/src/Main.java
+++ Adrian_COMP254_Lab01/src/Main.java
@@ -1,12 +1,28 @@
+import linkedlists.DoublyLinkedList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!!\n");
+        DoublyLinkedList<Integer> L = new DoublyLinkedList<>();
+        DoublyLinkedList<Integer> M = new DoublyLinkedList<>();
 
-        for (int i = 1; i <= 5; i++) {
+        // Populate L: [10, 20, 30]
+        L.addLast(10); L.addLast(20); L.addLast(30);
 
-            System.out.println("i = " + i);
-        }
+        // Populate M: [40, 50]
+        M.addLast(40); M.addLast(50);
+
+        System.out.print("List L: "); L.display();
+        System.out.print("List M: "); M.display();
+
+        L.concat(M);
+
+        System.out.print("Concatenated List L': ");
+        L.display();
+        System.out.println("New size: " + L.size());
     }
+
+
+
 }
